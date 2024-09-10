2565919841f2dcdabeca5e48c2e15193b5a13650
Pedro <48163181+neuralgremlin@users.noreply.github.com>
2024-09-09 20:46:14 -0600

    change wezterm setup

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -1,5 +1,13 @@
 local wezterm = require 'wezterm'
-local config = {}
-config.font = wezterm.font 'JetBrains Mono'
-config.font_size = 16
+local config = wezterm.config_builder()
+config = {
+    automatically_reload_config = true,
+    window_decorations = "RESIZE",
+    color_scheme = 'Catppuccin Mocha',
+    enable_tab_bar = false,
+    font = wezterm.font 'JetBrains Mono',
+    font_size = 16,
+    macos_window_background_blur = 20,
+    window_background_opacity = 0.8,
+}
 return config
