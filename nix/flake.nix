044e0ce4c7724556b239341b801d4a2017a67659
Pedro <48163181+neuralgremlin@users.noreply.github.com>
2025-09-29 23:37:23 -0600

    Update flake.nix

--- nix/flake.nix
+++ nix/flake.nix
@@ -73,6 +73,7 @@
           {
             home-manager.useGlobalPkgs = true;
             home-manager.useUserPackages = true;
+            home-manager.backupFileExtension = "hm-bak";
             home-manager.extraSpecialArgs = { user = currentUser; };
             home-manager.users.${currentUser} = import ./home/default.nix;
           }
